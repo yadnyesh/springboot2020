deaa578677f22d986a184d99ee4b6d0f643db217
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-01-14 14:59:10 +0530

    Spring Boot Actuator Working

--- src/main/java/io/yadnyesh/springboot2020/Springboot2020Application.java
+++ src/main/java/io/yadnyesh/springboot2020/Springboot2020Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot2020Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2020Application.class, args);
 	}
-
 }
