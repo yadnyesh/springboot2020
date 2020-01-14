bc1e94d9a8317c69c1a157d0fa1056b1bb1ed747
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-01-14 13:02:33 +0530

    first commit

---
+++ src/test/java/io/yadnyesh/springboot2020/Springboot2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.springboot2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
